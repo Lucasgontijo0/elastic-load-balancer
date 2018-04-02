ce108c1a3a262506929d4a75c1e8d07a6b34640d
Lucas Gontijo <Lucasgontijo0@gmail.com>
2018-04-02 14:45:26 -0300

    Refactoring

--- src/main/java/com/unitri/lucas/elasticloadbalancer/domain/Metrics.java
+++
@@ -1,8 +0,0 @@
-package com.unitri.lucas.elasticloadbalancer.domain;
-
-public interface Metrics {
-
-    void calculateArrivalRate();
-    void calculateServiceCharge();
-    void calculateAverageTimePerRequest();
-}
