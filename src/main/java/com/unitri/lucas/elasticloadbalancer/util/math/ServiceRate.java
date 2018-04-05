f170b3018bcfd30dcdcf3be1129d6c36a0ff2dab
Lucas Gontijo <Lucasgontijo0@gmail.com>
2018-04-05 13:25:32 -0300

    Service rate implementation.

--- src/main/java/com/unitri/lucas/elasticloadbalancer/util/math/ServiceRate.java
+++ src/main/java/com/unitri/lucas/elasticloadbalancer/util/math/ServiceRate.java
@@ -1,5 +1,28 @@
 package com.unitri.lucas.elasticloadbalancer.util.math;
 
+import com.unitri.lucas.elasticloadbalancer.util.math.representation.ServiceRepresentation;
+
+import java.sql.Timestamp;
+
 public class ServiceRate {
-    /* DEVE CALCULAR QUANTAS REQUISIÇÕES FORAM ATENDIDAS PELO PROXY (QUE TEM CHEGADA E SAÍDA.)*/
+
+    private long requestAmmount;
+    private Timestamp startDate;
+    private Timestamp endDate;
+
+    public ServiceRate(long requestAmmount, Timestamp startDate, Timestamp endDate) {
+        this.requestAmmount = requestAmmount;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public ServiceRepresentation calculateServiceRate(){
+
+        Long duration = endDate.getTime() - startDate.getTime();
+
+        double rate = this.requestAmmount / duration;
+
+        return new ServiceRepresentation(startDate, endDate, duration, this.requestAmmount, rate);
+
+    }
 }
--- src/main/java/com/unitri/lucas/elasticloadbalancer/util/math/representation/ServiceRepresentation.java
+++ src/main/java/com/unitri/lucas/elasticloadbalancer/util/math/representation/ServiceRepresentation.java
@@ -1,4 +1,40 @@
 package com.unitri.lucas.elasticloadbalancer.util.math.representation;
 
+import java.sql.Timestamp;
+
 public class ServiceRepresentation {
+
+    private Timestamp startDate;
+    private Timestamp endDate;
+    private long duration;
+    private Long ammount;
+    private Double arrivalRate;
+
+    public ServiceRepresentation(Timestamp startDate, Timestamp endDate, long duration, Long ammount, Double arrivalRate) {
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.duration = duration;
+        this.ammount = ammount;
+        this.arrivalRate = arrivalRate;
+    }
+
+    public Timestamp getStartDate() {
+        return startDate;
+    }
+
+    public Timestamp getEndDate() {
+        return endDate;
+    }
+
+    public long getDuration() {
+        return duration;
+    }
+
+    public Long getAmmount() {
+        return ammount;
+    }
+
+    public Double getArrivalRate() {
+        return arrivalRate;
+    }
 }
